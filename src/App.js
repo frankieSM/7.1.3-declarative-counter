d080056b746d478973feac88212faa3774be8e78
franc <fmenendezwork@gmail.com>
2023-07-22 16:00:56 -0700

    Added assignment to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  let [ counter, changeCounter ] = useState(1)
+  window.changeCounter = changeCounter //SHOULD NOT MAKE GLOBAL VARIABLES THIS WAY WITH A FINISHED APP. JUST USE FOR TESTING/DEBUG
+
+  return ( //return can only return ONE parent element. use div as a wrapper for the return.
+    <div> 
+      <h1>{counter}</h1>
+      <h2>{counter}</h2>
     </div>
-  );
+  )
 }
+
 
 export default App;
